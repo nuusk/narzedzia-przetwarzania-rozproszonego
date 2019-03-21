69ca4a630c73eecde02559c90b1e3d540bdf1438
Poe <piotr.a.ptak@icloud.com>
2019-03-21 12:31:59 +0100

    Remove unnecessary lines

--- zmq/client.c
+++ zmq/client.c
@@ -8,17 +8,17 @@
 
 int main(void)
 {
-  printf("Connecting to hello world server...\n");
+  printf("This client is about to send my index to unixlab server (~ D. Wawrzyniak)...\n");
   void *context = zmq_ctx_new();
   void *requester = zmq_socket(context, ZMQ_REQ);
   zmq_connect(requester, "tcp://150.254.30.38:5555");
 
   int indeks = 127347;
   char buffer[MAX];
-  printf("Sending indeks...\n");
+  printf("Sending index...\n");
   zmq_send(requester, &indeks, sizeof(int), 0);
   zmq_recv(requester, buffer, MAX, 0);
-  printf("End... %s\n", buffer);
+  printf("In response I got:\n%s\n", buffer);
   zmq_close(requester);
   zmq_ctx_destroy(context);
   return 0;
